e0f2e06af3de1f626abe5482b924ae817ff8a910
Vidyashankar93 <vidyashankar.giri@gmail.com>
2017-03-03 16:38:20 +0530

    Secound commit

--- HPE/src/Marmstrong.java
+++ HPE/src/Marmstrong.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Marmstrong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		 int n, sum = 0, temp, remainder, digits = 0;
 		 
 	      Scanner in = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class Marmstrong {
 	 
 	      temp = n;
 	 
-	      // Count number of digits
+	      
 	 
 	      while (temp != 0) {
 	         digits++;
